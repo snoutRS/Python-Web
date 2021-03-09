daf02e174a5f1f15c13dfcecaff3a03865ca7a4a
unknown <60055979+snoutRS@users.noreply.github.com>
2021-03-09 20:34:20 +0800

    参数传递
    罗列了各种参数传递方法

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+###app.py 是一个简单的基于flask框架登录验证系统对应的html文件是login.html
+###app1.py 是一个jinja2传值实，包括字典传值与对位传值，对应的网页为demo.html与demo1.html
--- app1.py
+++ app1.py
@@ -5,14 +5,18 @@ app = Flask(__name__)
 def hello_world():
     return 'hello_world'
 
-@app.route('/tempdemo')
+@app.route('/demo')
 def tempdemo():
     return render_template('demo.html',name='aaa',msg='asdf')
 
-@app.route('/tempdemo1')
+@app.route('/demo1')
 def tempdemo1():
     dict1={"name":"罗小黑","msg":"asdf"}
     return render_template('demo1.html',dict1=dict1)
+
+@app.route('/demo2')
+def tempdemo2():
+    return render_template('demo2.html',i = 123,s = 'abc',s_list=['abc','abb','aaa'],d={"name":'aaa',"age":43})
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1",port=80,debug=True)
